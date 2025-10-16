9cdff9eced4268f9da85edd14460bccec86bf57e
MagonxESP <janma.360@gmail.com>
2025-10-16 14:01:33 +0200

    chore: add missing license header

--- internal/common/logger.go
+++ internal/common/logger.go
@@ -1,3 +1,18 @@
+// AnimeAV1 Downloader - Un programa para extraer enlaces de descarga de animeav1.com
+// Copyright (C) 2025  MagonxESP
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Este programa se distribuye con la esperanza de que sea útil,
+// pero SIN NINGUNA GARANTÍA; sin siquiera la garantía implícita de
+// COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.
+// Consulte la Licencia Pública General de GNU para más detalles.
+//
+// Debe haber recibido una copia de la Licencia Pública General de GNU
+// junto con este programa.  Si no, consulte <https://www.gnu.org/licenses/>.
 package common
 
 import (
